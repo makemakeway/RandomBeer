6d146030aa733afa651b2b09c5a121ddf739919c
YeonBae Park <makemakeway@gmail.com>
2021-12-20 17:37:30 +0900

    First Commit

---
+++ RandomBeer/Controller/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  RandomBeer
+//
+//  Created by 박연배 on 2021/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    //MARK: Properties
+    
+    
+    
+    //MARK: UI
+    
+    
+    
+    //MARK: Method
+    
+    
+    
+    //MARK: LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .magenta
+    }
+
+
+}
+
